160f1f0f7417a086cb6c8332f71e860bbb56d00f
Lakshya Goel <lgoel340@gmail.com>
2023-11-10 23:28:57 +0530

    Quiz Application

---
+++ app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,29 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] = {
+            "What is the capital of India?",
+            "What is the capital of Australia?",
+            "What is the capital of USA?",
+            "What is the capital of UK?",
+            "What is the capital of Canada?"
+    };
+
+    public static String choices[][] = {
+            {"New Delhi", "Mumbai", "Kolkata", "Chennai"},
+            {"Sydney", "Melbourne", "Canberra", "Perth"},
+            {"New York", "Washington DC", "Los Angeles", "Chicago"},
+            {"London", "Manchester", "Liverpool", "Birmingham"},
+            {"Toronto", "Vancouver", "Ottawa", "Montreal"}
+    };
+
+    public static String correctAnswer[] = {
+            "New Delhi",
+            "Canberra",
+            "Washington DC",
+            "London",
+            "Ottawa"
+    };
+
+}
